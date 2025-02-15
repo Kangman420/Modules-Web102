e0f58a260908ef09c930269f2bde5294a1d81fcb
Kangman420 <tjrkd1024@gmail.com>
2025-02-15 15:07:09 +0900

    250215_3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Main from "./main";  // 파일명 대소문자 확인!
+import Main from "./Main";  // 파일명 대소문자 확인!
 import About from "./About";
 
 function App() {
